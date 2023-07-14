80cda503dc888802526f21afbf46020a2f6810df
Anmol2910 <anmolawasthi2910@gmail.com>
2023-07-14 17:04:14 +0530

    Now You can update name and email on user's profile

--- routes/user.js
+++ routes/user.js
@@ -8,6 +8,8 @@ const userController = require('../controllers/user_controller');
 
 router.get('/profile/:id', passport.checkAuthentication, userController.profile);
 
+router.post('/update/:id', passport.checkAuthentication, userController.update);
+
 router.get('/sign-up', userController.signUp);
 
 router.get('/sign-in', userController.signIn);
