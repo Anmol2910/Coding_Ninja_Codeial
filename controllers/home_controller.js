d760848b53621b4981475170687671caad0fa3a7
Anmol2910 <anmolawasthi2910@gmail.com>
2023-07-15 16:19:50 +0530

    Adding Async Await to some callbacks

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -2,38 +2,31 @@ const Post = require('../models/post');
 
 const User = require('../models/user')
 
-module.exports.home = function(req, res){
-    // console.log(req.cookies);
-    // res.cookie('user_id', 12);
+module.exports.home = async function(req, res){
+    // Populate the user of Each post
 
-    // Post.find({}, function(err, posts){
-
-    //     return res.render('home', {
-    //         title : 'Codieal | Home',
-    //         posts : posts
-    //     });
-    // });  
-
-// Populate the user of Each post
-    
-    Post.find({})
+    try{
+        let posts = await Post.find({})
     .populate('user')
     .populate({
         path: 'comments',
         populate : {
             path : 'user'
         }
-    })
-    .exec(function(err, posts){
+    });
 
-        User.find({}, function(err, users){
+    let users = await User.find({});
 
             return res.render('home', {
                 title : 'Codieal | Home',
                 posts : posts, 
                 all_users : users
             });
-        });
-    });  
-}
+
+    }catch(err){
+        console.log('Error', err);
+        return;
+
+    }
+ }
 
