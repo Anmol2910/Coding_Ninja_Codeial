82edad3023d2205550a8db9b1a1b5b21a1cfe091
Anmol2910 <anmolawasthi2910@gmail.com>
2023-07-17 17:37:26 +0530

    Deleting Post :: AJAX

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -12,6 +12,7 @@
                 success: function(data){
                     let newPost = newPostDom(data.data.post);
                     $('#posts-list-container>ul').prepend(newPost);
+                    deletePost($(' .delete-post-button', newPost));
                 }, error: function(error){
                     console.log(error.responseText);
                 }
@@ -25,7 +26,7 @@
         <p>
             
                 <small>
-                    <a class="delete-post-button" href="/posts/destroy/${post.id}">| DEL |</a>
+                    <a class="delete-post-button" href="/posts/destroy/${post._id}">| DEL |</a>
                 </small>
                
                     ${post.content}
@@ -55,6 +56,26 @@
         </div>
     
     </li>`)
+    }
+
+    // Method to delete a post from DOM
+
+    let deletePost = function(deleteLink){
+        $(deleteLink).click(function(e){
+            e.preventDefault();
+
+            $.ajax({
+                type: 'get',
+                url: $(deleteLink).prop('href'),
+                success: function(data){
+                    $(`#post-${data.data.post_id}`).remove();
+
+                },error: function(error){
+                    console.log(error.responseText);
+
+                }
+            })
+        })
     }
     createPost();
 }
