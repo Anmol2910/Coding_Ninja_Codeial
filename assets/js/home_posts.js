18168acbfa152d6832709e400caf3559535d738a
Anmol2910 <anmolawasthi2910@gmail.com>
2023-07-18 16:46:40 +0530

    Configuring Multer for Use Avatar

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -13,6 +13,21 @@
                     let newPost = newPostDom(data.data.post);
                     $('#posts-list-container>ul').prepend(newPost);
                     deletePost($(' .delete-post-button', newPost));
+
+                    // Call the create comment class
+
+                    new PostComments(data.data.post._id);
+
+                    new Noty({
+                        theme: 'relax',
+                        text: "Post published!",
+                        type: 'success',
+                        layout: 'topRight',
+                        timeout: 1500
+                        
+                    }).show();
+
+
                 }, error: function(error){
                     console.log(error.responseText);
                 }
@@ -70,6 +85,15 @@
                 success: function(data){
                     $(`#post-${data.data.post_id}`).remove();
 
+                    new Noty({
+                        theme: 'relax',
+                        text: "Post Deleted",
+                        type: 'success',
+                        layout: 'topRight',
+                        timeout: 1500
+                        
+                    }).show();
+
                 },error: function(error){
                     console.log(error.responseText);
 
@@ -77,5 +101,21 @@
             })
         })
     }
+
+     // loop over all the existing posts on the page (when the window loads for the first time) and call the delete post method on delete link of each, also add AJAX (using the class we've created) to the delete button of each
+     let convertPostsToAjax = function(){
+        $('#posts-list-container>ul>li').each(function(){
+            let self = $(this);
+            let deleteButton = $(' .delete-post-button', self);
+            deletePost(deleteButton);
+
+            // get the post's id by splitting the id attribute
+            let postId = self.prop('id').split("-")[1]
+            new PostComments(postId);
+        });
+    }
+
+
     createPost();
+    convertPostsToAjax();
 }
--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,53 +1,80 @@
 const Comment = require('../models/comment');
+const Post = require('../models/post');
 
-const Post = require('../models/post')
+module.exports.create = async function(req, res){
 
-module.exports.create = async function (req, res) {
-
-    try {
+    try{
         let post = await Post.findById(req.body.post);
-        if (post) {
+
+        if (post){
             let comment = await Comment.create({
                 content: req.body.content,
                 post: req.body.post,
                 user: req.user._id
             });
+
             post.comments.push(comment);
             post.save();
 
-            req.flash('success', 'Comment added Successfully');
+            if (req.xhr){
+                // Similar for comments to fetch the user's id!
+                comment = await comment.populate('user', 'name').execPopulate();
+    
+                return res.status(200).json({
+                    data: {
+                        comment: comment
+                    },
+                    message: "Post created!"
+                });
+            }
 
-            res.redirect('/')
-        };
-        } 
-    catch(err){
+
+            req.flash('success', 'Comment published!');
+
+            res.redirect('/');
+        }
+    }catch(err){
         req.flash('error', err);
-        return res.redirect('back');
+        return;
     }
+    
 }
 
 
-
-module.exports.destroy = async function (req, res) {
+module.exports.destroy = async function(req, res){
 
     try{
-
         let comment = await Comment.findById(req.params.id);
-        if (comment.user == req.user.id) {
+
+        if (comment.user == req.user.id){
+
             let postId = comment.post;
+
             comment.remove();
-    
-           let post = Post.findByIdAndUpdate(postId, { $pull: { comments: req.params.id }});
-          
+
+            let post = Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}});
+
+            // send the comment id which was deleted back to the views
+            if (req.xhr){
+                return res.status(200).json({
+                    data: {
+                        comment_id: req.params.id
+                    },
+                    message: "Post deleted"
+                });
+            }
+
+
+            req.flash('success', 'Comment deleted!');
+
             return res.redirect('back');
-        
-        }else {
+        }else{
+            req.flash('error', 'Unauthorized');
             return res.redirect('back');
         }
-    
     }catch(err){
-        console.log('Error', err);
+        req.flash('error', err);
         return;
     }
-  
+    
 }
