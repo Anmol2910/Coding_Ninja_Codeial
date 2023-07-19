9c276ebfaada58e72d7b9967d0d3965fc75b7c46
Anmol2910 <anmolawasthi2910@gmail.com>
2023-07-19 17:29:01 +0530

    Replacing the current Avatar

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,5 +1,10 @@
 const User = require("../models/user");
 
+const fs = require('fs');
+
+const path = require('path');
+
+
 module.exports.profile = function (req, res) {
     User.findById(req.params.id, function(err, user){
 
@@ -34,6 +39,12 @@ module.exports.update = async function(req, res){
 
                 if(req.file){
                     // this is saving the path of uploaded file into  the avatar field in the user
+
+                    if(user.avatar){
+                        fs.unlinkSync(path.join(__dirname, '..', user.avatar))
+                    }
+
+
                     user.avatar = User.avatarPath + '/' + req.file.filename;
                 }
                 user.save();
