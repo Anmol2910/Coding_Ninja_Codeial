ec83ccc91531c72ba86a86421d436874b9244ea9
Anmol2910 <anmolawasthi2910@gmail.com>
2023-07-28 15:59:13 +0530

    Adding Controller and Route for :: Likes

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,8 @@ router.get('/', homeController.home);
 router.use('/users', require('./user'));
 router.use('/posts', require('./posts'));
 router.use('/comments', require('./comments'));
+router.use('./likes', require('./likes'))
 router.use('/api', require('./api'));
+
 
 module.exports = router;
